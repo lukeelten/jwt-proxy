1059b5a6d0ca780badfabc4248d74cb7662f6ba3
Tobias Derksen <t.derksen@mailbox.org>
2022-09-13 17:08:16 +0200

    Supporting multiple keys

--- internal/jwt.go
+++ internal/jwt.go
@@ -1,18 +1,3 @@
-/*
-Copyright 2020 Gravitational, Inc.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-Source: https://github.com/gravitational/teleport/blob/v10.1.9/examples/jwt/verify-jwt.go
-*/
-
 package internal
 
 import (
@@ -20,11 +5,13 @@ import (
 	"crypto/tls"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"go.uber.org/zap"
 	"math/big"
 	"net/http"
-	"sync/atomic"
+	"strings"
+	"sync"
 	"time"
 
 	jwt "github.com/golang-jwt/jwt/v4"
@@ -32,7 +19,8 @@ import (
 
 type JWTValidator struct {
 	Logger          *zap.SugaredLogger
-	publicKey       atomic.Value
+	publicKeys      []Key
+	mutex           sync.RWMutex
 	jwksUrl         string
 	insecure        bool
 	shutdownChannel chan bool
@@ -43,6 +31,31 @@ type TeleportClaims struct {
 
 	Username string   `json:"username,omitempty"`
 	Roles    []string `json:"roles,omitempty"`
+}
+
+type Key struct {
+	Id  string
+	Key *rsa.PublicKey
+}
+
+// jwk is a JSON Web Key, described in detail in RFC 7517.
+type jwk struct {
+	KeyId string `json:"kid,omitempty"`
+	// KeyType is the type of asymmetric key used.
+	KeyType string `json:"kty"`
+	// Algorithm used to sign.
+	Algorithm string `json:"alg"`
+	Usage     string `json:"use,omitempty"`
+	// N is the modulus of the public key.
+	N string `json:"n"`
+	// E is the exponent of the public key.
+	E string `json:"e"`
+}
+
+// jwksResponse is the response format for the JWK endpoint.
+type jwksResponse struct {
+	// Keys is a list of public keys in JWK format.
+	Keys []jwk `json:"keys"`
 }
 
 func NewJWTValidator(config TeleportConfig, logger *zap.SugaredLogger) *JWTValidator {
@@ -79,80 +92,88 @@ func (jva *JWTValidator) Shutdown() {
 }
 
 func (jva *JWTValidator) refreshKey() {
-	jva.Logger.Info("Loading JWKS Key", "url", jva.jwksUrl, "insecure", jva.insecure)
+	jva.Logger.Infow("Loading JWKS Key", "url", jva.jwksUrl, "insecure", jva.insecure)
 
-	key, err := getPublicKey(jva.jwksUrl, jva.insecure)
+	keys, err := jva.loadKeys()
 	if err != nil {
 		jva.Logger.Fatalw("cannot read public keys", "err", err, "url", jva.jwksUrl, "insecure", jva.insecure)
 	}
 
-	jva.Logger.Debugw("Got key", "key", key)
+	jva.Logger.Debugw("Got keys", "keys", keys)
 
-	jva.publicKey.Store(key)
+	jva.mutex.Lock()
+	defer jva.mutex.Unlock()
+	jva.publicKeys = keys
 }
 
 func (jva *JWTValidator) Validate(tokenString string) (*TeleportClaims, error) {
 	keyFunc := func(token *jwt.Token) (interface{}, error) {
-		return jva.publicKey.Load(), nil
+		jva.mutex.RLock()
+		defer jva.mutex.RUnlock()
+
+		kidValue, okGet := token.Header["kid"]
+		kid, okCast := kidValue.(string)
+		if !okGet || !okCast || len(kid) == 0 {
+			if len(jva.publicKeys) > 0 {
+				return jva.publicKeys[0].Key, nil
+			} else {
+				jva.Logger.Debugw("cannot find any matching key", "token", token, "keys", jva.publicKeys)
+				return nil, errors.New("cannot find suitable key")
+			}
+		}
+
+		for _, key := range jva.publicKeys {
+			if strings.Compare(kid, key.Id) == 0 {
+				return key.Key, nil
+			}
+		}
+
+		jva.Logger.Debugw("cannot find requested key", "token", token, "keys", jva.publicKeys)
+		return nil, errors.New("cannot find required key")
 	}
 
 	var claims TeleportClaims
 	_, err := jwt.ParseWithClaims(tokenString, &claims, keyFunc)
 	if err != nil {
+		jva.Logger.Debugw("cannot validate token", "err", err, "claims", claims, "token", tokenString)
 		return nil, err
 	}
 
 	return &claims, claims.Valid()
 }
 
-// jwk is a JSON Web Key, described in detail in RFC 7517.
-type jwk struct {
-	// KeyType is the type of asymmetric key used.
-	KeyType string `json:"kty"`
-	// Algorithm used to sign.
-	Algorithm string `json:"alg"`
-	// N is the modulus of the public key.
-	N string `json:"n"`
-	// E is the exponent of the public key.
-	E string `json:"e"`
+func (key jwk) getKey() (*rsa.PublicKey, error) {
+	n, err := base64.RawURLEncoding.DecodeString(key.N)
+	if err != nil {
+		return nil, err
+	}
+
+	e, err := base64.RawURLEncoding.DecodeString(key.E)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rsa.PublicKey{
+		N: new(big.Int).SetBytes(n),
+		E: int(new(big.Int).SetBytes(e).Uint64()),
+	}, nil
 }
 
-// jwksResponse is the response format for the JWK endpoint.
-type jwksResponse struct {
-	// Keys is a list of public keys in JWK format.
-	Keys []jwk `json:"keys"`
-}
-
-// claims represents public and private claims for a JWT token.
-type claims struct {
-	// Claims represents public claim values (as specified in RFC 7519).
-	jwt.Claims
-
-	// Username returns the Teleport identity of the user.
-	Username string `json:"username"`
-
-	// Roles returns the list of roles assigned to the user within Teleport.
-	Roles []string `json:"roles"`
-}
-
-// @todo support for multiple keys
-// getPublicKey fetches the public key from the JWK endpoint.
-func getPublicKey(url string, insecureSkipVerify bool) (*rsa.PublicKey, error) {
+func (jva *JWTValidator) loadKeys() ([]Key, error) {
 	// Fetch JWKs.
 	client := &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: insecureSkipVerify,
+				InsecureSkipVerify: jva.insecure,
 			},
 		},
 	}
-	resp, err := client.Get(url)
+	resp, err := client.Get(jva.jwksUrl)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	// Parse JWKs response.
 	var response jwksResponse
 	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
 		return nil, err
@@ -161,18 +182,20 @@ func getPublicKey(url string, insecureSkipVerify bool) (*rsa.PublicKey, error) {
 		return nil, fmt.Errorf("no keys found")
 	}
 
-	// Construct a crypto.PublicKey from the response.
-	jwk := response.Keys[0]
-	n, err := base64.RawURLEncoding.DecodeString(jwk.N)
-	if err != nil {
-		return nil, err
+	keys := make([]Key, 0)
+	for _, k := range response.Keys {
+		if strings.Compare(strings.ToUpper(k.Algorithm), "RSA") != 0 {
+			// We only support RSA
+			continue
+		}
+
+		publickey, err := k.getKey()
+		if err != nil {
+			return nil, err
+		}
+
+		keys = append(keys, Key{Key: publickey, Id: k.KeyId})
 	}
-	e, err := base64.RawURLEncoding.DecodeString(jwk.E)
-	if err != nil {
-		return nil, err
-	}
-	return &rsa.PublicKey{
-		N: new(big.Int).SetBytes(n),
-		E: int(new(big.Int).SetBytes(e).Uint64()),
-	}, nil
+
+	return keys, nil
 }
